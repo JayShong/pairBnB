16071550cc70c9c598fb0e46397c83937cd2ff0c
Jay <jayshong@gmail.com>
2016-11-25 10:14:46 +0800

    User table added avatars column Model user and amenity added links for avatars column View for user form modified to include uploading

--- pair_bnb/db/schema.rb
+++ pair_bnb/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124085756) do
+ActiveRecord::Schema.define(version: 20161125021123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20161124085756) do
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128,             null: false
     t.integer  "role",                           default: 0
+    t.json     "avatars"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
