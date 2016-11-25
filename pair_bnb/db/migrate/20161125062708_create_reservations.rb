49da24445bae63a6202c45a5d10a4ebd794a283a
Jay <jayshong@gmail.com>
2016-11-25 14:29:46 +0800

    Created a reservations table

---
+++ pair_bnb/db/migrate/20161125062708_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.integer :iisting_id
+      t.integer :user_id
+      t.date :check_in_date
+      t.date :check_out_date
+      t.string :comments, :default => 0
+
+      t.timestamps null: false
+    end
+  end
+end
