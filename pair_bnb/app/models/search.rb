74f418f22845c2f535ddb1b96d76e9d04bf6152e
Jay <jayshong@gmail.com>
2016-11-30 15:50:08 +0800

    debugged to search through the first results for prices

--- pair_bnb/app/models/search.rb
+++ pair_bnb/app/models/search.rb
@@ -7,7 +7,21 @@ class Search < ActiveRecord::Base
 				input.delete("#{keys[0]}")
 			end
 		end
-		return input
+		output = input.symbolize_keys
+		if output[:low_price]
+			output.delete(:low_price)
+		end
+		if output[:high_price]
+			output.delete(:high_price)
+		end
+		return output
+
+		# {"address"=>"Jalan Testing 5"}
+
 	end
 
+	def self.query_search_builder(filter_params)
+		# filter_params = {:address=>"Jalan", :location=>"test", :low_price=>"300", :high_price=>"500"}
+
+	end
 end
